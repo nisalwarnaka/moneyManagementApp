41c4c5692edefdc0b95c743dc69da5839af102ea
nisal <nisalwarnka.1999@gmail.com>
2025-07-07 15:38:35 +0530

    Dashboard UI created

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,7 @@
                     @endauth
                 </nav>
             @endif
+            @vite('resources/css/app.css')
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
@@ -269,7 +270,6 @@
                 </div>
             </main>
         </div>
-
         @if (Route::has('login'))
             <div class="h-14.5 hidden lg:block"></div>
         @endif
