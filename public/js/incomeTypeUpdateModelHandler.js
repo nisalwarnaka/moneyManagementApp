da7a177ee6796c1e194a2e6b5dfe57d35acd6e31
nisal <nisalwarnka.1999@gmail.com>
2025-07-10 14:58:20 +0530

    Create delete income type UI

---
+++ public/js/incomeTypeUpdateModelHandler.js
@@ -0,0 +1,17 @@
+function openEditModal(button){
+    document.getElementById('incomeTypeIdHeader').textContent = button.getAttribute('data-id');
+    document.getElementById('incomeTypeNameHeader').textContent = button.getAttribute('data-income_type');
+    document.getElementById('incomeType').value = button.getAttribute('data-income_type');
+    document.getElementById('maxAmount').value = button.getAttribute('data-max_amount');
+    document.getElementById('minAmount').value = button.getAttribute('data-min_amount');
+    document.getElementById('incomeId').value = button.getAttribute('data-id');
+}
+
+function openDeleteModal(button)
+{
+    document.getElementById('incomeTypeShowInHeader').textContent = button.getAttribute('data-income_type');
+    document.getElementById('incomeIdShowInHeader').textContent = button.getAttribute('data-id');
+    document.getElementById('incomeIdDelete').value = button.getAttribute('data-id');
+}
+
+
