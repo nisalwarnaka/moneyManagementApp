53e0355dde07af4c158d3bb10af61df3b510f17e
nisal <nisalwarnka.1999@gmail.com>
2025-07-15 01:24:42 +0530

    Fix bug in income type editor & expense type editor, add new validation code.

--- public/js/incomeTypeUpdateModelHandler.js
+++ public/js/incomeTypeUpdateModelHandler.js
@@ -1,7 +1,7 @@
 function openEditModal(button){
     document.getElementById('incomeTypeIdHeader').textContent = button.getAttribute('data-id');
     document.getElementById('incomeTypeNameHeader').textContent = button.getAttribute('data-income_type');
-    document.getElementById('incomeType').value = button.getAttribute('data-income_type');
+    document.getElementById('incomeTypeModel').value = button.getAttribute('data-income_type');
     document.getElementById('maxAmount').value = button.getAttribute('data-max_amount');
     document.getElementById('minAmount').value = button.getAttribute('data-min_amount');
     document.getElementById('incomeId').value = button.getAttribute('data-id');
