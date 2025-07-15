c12a1fcb528d67588d770a96ace89bd77002d166
nisal <nisalwarnka.1999@gmail.com>
2025-07-16 03:06:51 +0530

    Create expense edit with js, defined route,controller & request

---
+++ public/js/expenseTransactionEditAndDeleteModelHandler.js
@@ -0,0 +1,18 @@
+function openExpenseTransactionEditModal(button){
+    document.getElementById('ExpenseTransactionIdForEditeModelText').textContent = button.getAttribute('data-expense-transaction-id');
+    document.getElementById('ExpenseTypeForEditeModelText').textContent = button.getAttribute('data-expense-type');
+    document.getElementById('ExpenseValueForEditModel').value = button.getAttribute('data-expense-value');
+    document.getElementById('ExpenseMonthForEditModel').value = button.getAttribute('data-expense-transaction-month');
+    document.getElementById('ExpenseSpecialNoteForEditModel').value = button.getAttribute('data-expense-special-note');
+    document.getElementById('ExpenseTransactionIdForEditModel').value = button.getAttribute('data-expense-transaction-id');
+
+}
+
+function openExpenseTransactionDeleteModal(button)
+{
+    document.getElementById('incomeTransactionTypeForText').textContent = button.getAttribute('data-income-type');
+    document.getElementById('incomeTransactionIdForText').textContent = button.getAttribute('data-transaction-id');
+    document.getElementById('incomeTransactionIdForDelete').value = button.getAttribute('data-transaction-id');
+}
+
+
---
+++ resources/views/profile/partials/show-expense-transactions.blade.php
@@ -0,0 +1,221 @@
+<section class="space-y-6">
+
+    <div class="md:gap-6 ">
+
+        <form action="{{Route('findExpense.find')}}" method="post">
+            @csrf
+            <div class="grid md:grid-cols-4 md:gap-6">
+                <div class="relative z-0 w-full mb-5 group">
+                    <select name="ExpenseTypeForFinder" id="ExpenseTypeForFinder" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
+
+                        <option value="" >Expense Type</option>
+                        @foreach($allExpenseData as $allExpenseTypes)
+                            <option value="{{$allExpenseTypes->id}}" data-name ="{{$allExpenseTypes->expense_type}}">{{$allExpenseTypes->expense_type}}</option>
+                        @endforeach
+
+                    </select>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <select name="ExpenseMonthForFinder" id="ExpenseMonthForFinder" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
+
+                        <option value="" >Month</option>
+                        <option value="January">January</option>
+                        <option value="February">February</option>
+                        <option value="March">March</option>
+                        <option value="April">April</option>
+                        <option value="May">May</option>
+                        <option value="June">June</option>
+                        <option value="July">July</option>
+                        <option value="August">August</option>
+                        <option value="September">September</option>
+                        <option value="November">November</option>
+                        <option value="December">December</option>
+                    </select>
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <input type="date" name="ExpenseDateForFinder" id="ExpenseDateForFinder" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+
+                </div>
+                <div class="relative z-0 w-full mb-5 group">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-1.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Find Transactions</button>
+                </div>
+            </div>
+        </form>
+        <br>
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-900 uppercase bg-gray-50 dark:bg-gray-200 dark:text-gray-900">
+                <tr>
+                    <th scope="col" class="px-6 py-3">
+                        Expense type
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Value
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Special note
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Month
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Date & time
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Trans id
+                    </th>
+                    <th scope="col" class="px-4 py-3">
+                        Action
+                    </th>
+                    <th scope="col" class="px-4 py-3">
+                        Action
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                @if($results->count())
+
+                    @foreach($results as $filteredResult)
+                        <tr class="bg-white border-b dark:bg-gray-100 dark:border-gray-700 border-gray-200">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-gray-900">
+                                {{$filteredResult->expense_type}}
+                            </th>
+                            <td class="px-6 py-4 text-gray-900">
+                                {{$filteredResult->expense_value}}
+                            </td>
+                            <td class="px-6 py-4 text-gray-900">
+                                {{$filteredResult->special_note}}
+                            </td>
+                            <td class="px-6 py-4 text-gray-900">
+                                {{$filteredResult->month}}
+                            </td>
+                            <td class="px-6 py-4 text-gray-900">
+                                {{$filteredResult->created_at->format('M j, Y')}}
+                            </td>
+                            <td class="px-6 py-4 text-gray-900">
+                                {{$filteredResult->id}}
+                            </td>
+                            <td class="px-6 py-4">
+                                <button data-expense-transaction-id="{{$filteredResult->id}}"
+                                        data-expense-type="{{$filteredResult->expense_type}}"
+                                        data-expense-value="{{$filteredResult->expense_value}}"
+                                        data-expense-transaction-month="{{$filteredResult->month}}"
+                                        data-expense-special-note="{{$filteredResult->special_note}}" data-modal-target="expenseTransactionEditModel" data-modal-toggle="expenseTransactionEditModel" onclick="openExpenseTransactionEditModal(this)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button >
+                            </td>
+                            <td class="px-6 py-4">
+                                <button data-expense-transaction-id="{{$filteredResult->id}}"
+                                        data-expense-type="{{$filteredResult->expense_type}}" data-modal-target="ExpenseTransactionDeleteModel" data-modal-toggle="ExpenseTransactionDeleteModel" onclick="openExpenseTransactionDeleteModal(this)" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
+                            </td>
+
+
+                        </tr>
+                    @endforeach
+
+                @endif
+                </tbody>
+            </table>
+
+            <!-- Main modal -->
+            <div id="expenseTransactionEditModel" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                <div class="relative p-4 w-full max-w-2xl max-h-full">
+                    <!-- Modal content -->
+                    <div class="relative rounded-lg shadow-sm dark:bg-blue-100">
+                        <!-- Modal header -->
+                        <form action="{{Route('editExpenseTransaction.edit')}}" method="post">
+                            @csrf
+                            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                                <h3 id="ExpenseTypeForEditeModelText" class="text-xl font-semibold dark:text-gray-900">
+
+                                </h3>
+                                <h3 class="text-xl font-semibold dark:text-blue-100">
+                                    ----
+                                </h3>
+                                <h3 id="ExpenseTransactionIdForEditeModelText" class="text-xl font-semibold dark:text-gray-900">
+
+                                </h3>
+                                <button type="button" class="text-gray-900 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="expenseTransactionEditModel">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                            </div>
+                            <!-- Modal body -->
+                            <div class="p-4 md:p-5 space-y-4">
+
+                                <div class="grid md:grid-cols-2 md:gap-6">
+                                    <div class="relative z-0 w-full mb-5 group">
+                                        <input type="number" name="ExpenseValueForEditModel" id="ExpenseValueForEditModel" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                        <label for="ExpenseValueForEditModel" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-blue-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Transaction Value</label>
+                                    </div>
+                                    <div class="relative z-0 w-full mb-5 group">
+                                        <select name="ExpenseMonthForEditModel" id="ExpenseMonthForEditModel" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-gray-900 dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" >
+
+                                            <option value="" >Month</option>
+                                            <option value="January">January</option>
+                                            <option value="February">February</option>
+                                            <option value="March">March</option>
+                                            <option value="April">April</option>
+                                            <option value="May">May</option>
+                                            <option value="June">June</option>
+                                            <option value="July">July</option>
+                                            <option value="August">August</option>
+                                            <option value="September">September</option>
+                                            <option value="November">November</option>
+                                            <option value="December">December</option>
+                                        </select>
+                                    </div>
+
+                                </div>
+                                <div class="relative z-0 w-full mb-5 group">
+                                    <textarea id="ExpenseSpecialNoteForEditModel" name="ExpenseSpecialNoteForEditModel" rows="4" class="block p-2.5 w-full text-sm rounded-lg border  focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-900 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Special Note"></textarea>
+                                    <input type="hidden" id="ExpenseTransactionIdForEditModel" name="ExpenseTransactionIdForEditModel" >
+
+
+                                </div>
+
+                            </div>
+                            <!-- Modal footer -->
+                            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+                                <button data-modal-hide="expenseTransactionEditModel" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</button>
+                                <button data-modal-hide="expenseTransactionEditModel" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Cansel</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+                <form action="" method="post" onsubmit="return confirm('Are you sure? want to delete this Expense transaction !');">
+                    @csrf
+                    <div id="ExpenseTransactionDeleteModel" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                        <div class="relative p-4 w-full max-w-md max-h-full">
+                            <div class="relative bg-white rounded-lg shadow-sm dark:bg-red-500">
+                                <button type="button" class="absolute top-3 end-2.5 text-white bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="ExpenseTransactionDeleteModel">
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span class="sr-only">Close modal</span>
+                                </button>
+                                <div class="p-4 md:p-5 text-center">
+                                    <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                                    </svg>
+                                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-white">Are you sure you want to delete this<br><span id=""></span><span class="dark:text-red-500"> -</span>: <span id=""></span><br> Income Type ?</h3>
+
+                                    <input type="text" name="" id="" required />
+
+                                    <button data-modal-hide="ExpenseTransactionDeleteModel" type="button" class="text-white bg-red-700  dark:border-white hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                                        Delete
+                                    </button>
+                                    <button data-modal-hide="ExpenseTransactionDeleteModel" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-white dark:border-white dark:hover:text-white dark:hover:bg-gray-700">Cancel</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
+
+        </div>
+    </div>
+    <script src="{{ asset('js/expenseTransactionEditAndDeleteModelHandler.js') }}"></script>
+</section>
